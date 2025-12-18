1ccd4ed937715008391da905b8e898200e7d010b
akmaalsyh <taufansyah50@gmail.com>
2025-12-18 20:48:28 +0700

    feat: Menambahkan RepositoryDataSiswa

---
+++ app/src/main/java/com/example/questapi_236/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,16 @@
+package com.example.questapi_236.repositori
+
+import com.example.questapi_236.apiservice.ServiceApiSiswa
+import com.example.questapi_236.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
