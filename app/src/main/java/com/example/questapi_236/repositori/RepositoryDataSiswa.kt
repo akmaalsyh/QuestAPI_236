0f22ff09ea5056055c8ff6bd35f049919101fd6b
akmaalsyh <taufansyah50@gmail.com>
2025-12-25 18:50:09 +0700

    Menambahkan fungsi CRUD ke RepositoriDataSiswa

--- app/src/main/java/com/example/questapi_236/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_236/repositori/RepositoryDataSiswa.kt
@@ -1,41 +1,32 @@
-package com.example.questapi_236.repositori
+package com.questapi_236.repositori
 
-import com.example.questapi_236.apiservice.ServiceApiSiswa
-import com.example.questapi_236.modeldata.DataSiswa
+import com.questapi_236.apiservice.ServiceApiSiswa
+import com.questapi_236.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-
-    // Di Interface
-    suspend fun getSiswaById(id: Int): DataSiswa
-    suspend fun deleteSiswa(id: Int)
-
-    suspend fun updateSiswa(id: Int, dataSiswa: DataSiswa)
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
-) : RepositoryDataSiswa{
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-    // Di JaringanRepositoryDataSiswa
-    override suspend fun getSiswaById(id: Int): DataSiswa {
-        return serviceApiSiswa.getSiswaById(id)
-    }
+) : RepositoryDataSiswa {
 
-    override suspend fun deleteSiswa(id: Int) {
-        try {
-            val response = serviceApiSiswa.deleteSiswa(id)
-            if (!response.isSuccessful) {
-                throw Exception("Gagal menghapus data di server")
-            }
-        } catch (e: Exception) {
-            throw e
-        }
-    }
+    override suspend fun getDataSiswa(): List<DataSiswa> =
+        serviceApiSiswa.getSiswa()
 
-    override suspend fun updateSiswa(id: Int, dataSiswa: DataSiswa) {
-        serviceApiSiswa.updateSiswa(id, dataSiswa)
-    }
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+
+    override suspend fun getSatuSiswa(id: Int): DataSiswa =
+        serviceApiSiswa.getSatuSiswa(id)
+
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
 }
