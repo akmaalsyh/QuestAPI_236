94bcc0cb4027515dac980237841c58a198bf93e8
akmaalsyh <taufansyah50@gmail.com>
2025-12-25 18:48:07 +0700

    Mengganti nama DestinasiDetailSiswa menjadi DestinasiDetail dan menambahkan route argument

--- app/src/main/java/com/example/questapi_236/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_236/uicontroller/route/DestinasiDetail.kt
@@ -2,8 +2,9 @@ package com.example.questapi_236.uicontroller.route
 
 import com.example.questapi_236.R
 
-// Tambahkan objek destinasi baru
 object DestinasiDetail : DestinasiNavigasi {
-    override val route = "detail"
+    override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
