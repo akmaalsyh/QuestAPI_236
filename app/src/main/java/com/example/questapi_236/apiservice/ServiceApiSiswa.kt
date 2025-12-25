9c72dde7ae9807bdc7adf77aadb3905b0a0f3c29
akmaalsyh <taufansyah50@gmail.com>
2025-12-25 18:50:19 +0700

    Mengubah nama endpoint dan metode di ServiceApiSiswa

--- app/src/main/java/com/example/questapi_236/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_236/apiservice/ServiceApiSiswa.kt
@@ -1,28 +1,32 @@
-package com.example.questapi_236.apiservice
+package com.questapi_236.apiservice
 
-import com.example.questapi_236.modeldata.DataSiswa
-import retrofit2.Response
+import com.questapi_236.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.Response
 import retrofit2.http.Query
 
 interface ServiceApiSiswa {
+
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
-    suspend fun postSiswa(
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): Response<Void>
+
+    @GET("baca1Teman.php")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php")
+    suspend fun editSatuSiswa(
+        @Query("id") id: Int,
         @Body dataSiswa: DataSiswa
-    ): Response<Void>
+    ): retrofit2.Response<Void>
 
-    @GET("getSiswaById.php")
-    suspend fun getSiswaById(@Query("id") id: Int): DataSiswa
-
-    @DELETE("deleteTM.php") // Sesuaikan nama file PHP kamu
-    suspend fun deleteSiswa(@Query("id") id: Int): retrofit2.Response<Void>
-
-    @POST("updateTM.php")
-    suspend fun updateSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+    @DELETE("deleteTM.php")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int): retrofit2.Response<Void>
 }
